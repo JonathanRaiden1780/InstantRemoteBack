57465f541526f9115a22bd7f5acebb220fbf5f41
JonathanRaiden1780 <44815477+JonathanRaiden1780@users.noreply.github.com>
2025-01-30 09:28:11 -0600

    build

--- InstantRemote.Services/obj/Debug/net8.0/InstantRemote.Services.AssemblyInfo.cs
+++ InstantRemote.Services/obj/Debug/net8.0/InstantRemote.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstantRemote.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da8d3df53d31a4a530a9b4b7bbb6c675e296ff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea944ff864825e8086593b88141688b8a3075be")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstantRemote.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstantRemote.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
