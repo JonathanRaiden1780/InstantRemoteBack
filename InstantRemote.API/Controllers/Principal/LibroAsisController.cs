51cbb5f0f6a80c6130fb6382a8d1e173e4af4446
JonathanRaiden1780 <44815477+JonathanRaiden1780@users.noreply.github.com>
2025-03-28 18:49:40 -0600

    parte 1 lbiro asis

---
+++ InstantRemote.API/Controllers/Principal/LibroAsisController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InstantRemote.Core.Helpers;
+using InstantRemote.Core.Exceptions;
+using InstantRemote.Core.Contracts.Factories.Common;
+using req = InstantRemote.Core.Dtos.Common.Request;
+using res = InstantRemote.Core.Dtos.Common.Response;
+using InstantRemote.Core.Dtos;
+using InstantRemote.Core.EntitiesStore.Common;
+using Microsoft.AspNetCore.Authorization;
+
+namespace InstantRemote.Api.Controllers.Principal
+{
+    [ApiController]
+    [Produces(Constants.ContentType)]
+    [Route(Constants.RouteDevices, Name = Constants.Devices)]
+    public class DevicesController : BaseController
+    {
+        public DevicesController(Func<string, IServiceFactory> serviceFactory) : base(serviceFactory)
+        {
+        }
+        
+        [HttpGet(Constants.GetDispositivos)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(BadRequestDto), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(res.FunctionalErrorMessageDto), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(res.CriticalErrorMessageDto), StatusCodes.Status500InternalServerError)]
+        public ActionResult GetDispositivos(int emplid)
+        {
+            ActionResult result;
+            try
+            {
+                var response = serviceFactory("IR").ServicePrincipal.GetDispositivos(emplid);
+                result = Ok(response);
+            }
+
+            catch (BusinessException busex)
+            {
+                var trackingCode = new Guid().ToString();
+                result = Conflict(new res.FunctionalErrorMessageDto
+                {
+                    Origin = Constants.OriginService, Message = new[] {busex.Message}, Url = Redirect404,
+                    TrackingCode = trackingCode
+                });
+            }
+            catch (Exception ex)
+            {
+                var trackingCode = new Guid().ToString();
+                result = StatusCode(StatusCodes.Status500InternalServerError,
+                    new res.CriticalErrorMessageDto
+                    {
+                        Origin = Constants.OriginService, Message = new[] {ex.ToString()}, TrackingCode = trackingCode
+                    });
+            }
+            finally
+            {
+            }
+
+            return result;
+        }
+        
+        [HttpGet(Constants.GetDispositivoDetalle)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(BadRequestDto), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(res.FunctionalErrorMessageDto), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(res.CriticalErrorMessageDto), StatusCodes.Status500InternalServerError)]
+        public ActionResult GetDispositivoDetalle(string serie)
+        {
+            ActionResult result;
+            try
+            {
+                var response = serviceFactory("IR").ServicePrincipal.GetDispositivoDetalle(serie);
+                result = Ok(response);
+            }
+
+            catch (BusinessException busex)
+            {
+                var trackingCode = new Guid().ToString();
+                result = Conflict(new res.FunctionalErrorMessageDto
+                {
+                    Origin = Constants.OriginService, Message = new[] {busex.Message}, Url = Redirect404,
+                    TrackingCode = trackingCode
+                });
+            }
+            catch (Exception ex)
+            {
+                var trackingCode = new Guid().ToString();
+                result = StatusCode(StatusCodes.Status500InternalServerError,
+                    new res.CriticalErrorMessageDto
+                    {
+                        Origin = Constants.OriginService, Message = new[] {ex.ToString()}, TrackingCode = trackingCode
+                    });
+            }
+            finally
+            {
+            }
+
+            return result;
+        }
+        
+       
+    }
+}
